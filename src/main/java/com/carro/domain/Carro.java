c00e7277d045e272c4f16ed3798a29a9bd18e82f
Gabriel Luiz Alves da Silva Deina <gabrieldeina@Gabriels-MacBook-Pro.local>
2021-07-15 12:33:54 -0300

    fixed error

--- src/main/java/com/carro/domain/Carro.java
+++ src/main/java/com/carro/domain/Carro.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
@@ -16,10 +17,10 @@ public class Carro {
 	private Long id;
 	private String nome;
 	private String tipo;
-	private String descricao;
-	private String urlFoto;
-	private String urlVideo;
-	private String latitude;
-	private String longitude;
+//	private String descricao;
+//	private String urlFoto;
+//	private String urlVideo;
+//	private String latitude;
+//	private String longitude;
 	
 }
