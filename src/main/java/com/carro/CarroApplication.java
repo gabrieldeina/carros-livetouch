3197e42f083de712db31d25230327d58b68a8445
Gabriel Luiz Alves da Silva Deina <gabrieldeina@Gabriels-MacBook-Pro.local>
2021-07-15 13:17:39 -0300

    teste2

--- src/main/java/com/carro/CarroApplication.java
+++ src/main/java/com/carro/CarroApplication.java
@@ -3,7 +3,7 @@ package com.carro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.carro"})
 public class CarroApplication {
 
 	public static void main(String[] args) {
